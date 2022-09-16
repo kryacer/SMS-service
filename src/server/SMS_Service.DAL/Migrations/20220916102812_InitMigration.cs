58b43e7da51f22a9c27ee36d387dd5b4ae4e5a68
Askhat Tuyenbayev <tm.askhat@ya.ru>
2022-09-16 16:29:06 +0600

    initial migration

---
+++ src/server/SMS_Service.DAL/Migrations/20220916102812_InitMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS_Service.DAL.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SMS",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    From = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SMS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SmsToReceivers",
+                columns: table => new
+                {
+                    SmsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReceiverNumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SmsToReceivers", x => new { x.SmsId, x.ReceiverNumber });
+                    table.ForeignKey(
+                        name: "FK_SmsToReceivers_SMS_SmsId",
+                        column: x => x.SmsId,
+                        principalTable: "SMS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SmsToReceivers");
+
+            migrationBuilder.DropTable(
+                name: "SMS");
+        }
+    }
+}
